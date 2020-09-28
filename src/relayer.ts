116f81ce37063e3eaa3b8ac619654a47ed6e17ec
Igor Yalovoy <to@ylv.io>
2020-09-28 23:10:49 +0300

    Revert "Add gasPrice field to send-tx payload"
    This reverts commit 40c94a136c71564e9739c2d7bd1c0c0b45b2fb19.

--- src/relayer.ts
+++ src/relayer.ts
@@ -1,6 +1,7 @@
 import { authenticate } from './auth';
 import { createApi } from './api';
 import { AxiosInstance } from 'axios';
+import util from 'util';
 
 export type Address = string;
 export type BigUInt = string | number;
@@ -8,24 +9,13 @@ export type Hex = string;
 export type Speed = 'safeLow' | 'average' | 'fast' | 'fastest';
 export type Status = 'pending' | 'sent' | 'submitted' | 'inmempool' | 'mined' | 'confirmed';
 
-import { OneOf, XOR } from './utility-types';
-
-export type RelayerTransactionPayload = XOR<
-  {
-    to: Address;
-    value?: BigUInt;
-    data?: Hex;
-    gasLimit: BigUInt;
-    speed?: Speed;
-  },
-  {
-    to: Address;
-    value?: BigUInt;
-    data?: Hex;
-    gasLimit: BigUInt;
-    gasPrice: BigUInt;
-  }
->;
+export type RelayerTransactionPayload = {
+  to: Address;
+  value?: BigUInt;
+  data?: Hex;
+  speed?: Speed;
+  gasLimit: BigUInt;
+};
 
 export interface SignMessagePayload {
   message: Hex;
