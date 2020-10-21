38f3479921011385e33796b0b28b39237b4ed931
Igor Yalovoy <to@ylv.io>
2020-10-21 15:50:48 -0300

    Throw init error at methods; More tests (#11)
    * Throw init error at methods; More tests
    
    * Typo

--- src/relayer.test.ts
+++ src/relayer.test.ts
@@ -8,10 +8,16 @@ jest.mock('./auth');
 jest.mock('./api');
 jest.mock('aws-sdk');
 
-type TestApiRelayer = Omit<ApiRelayer, 'api'> & { api: AxiosInstance };
+type TestApiRelayer = Omit<ApiRelayer, 'api'> & {
+  api: AxiosInstance;
+  apiKey: string;
+  apiSecret: string;
+  init: () => Promise<void>;
+};
 
 describe('ApiRelayer', () => {
   let relayer: TestApiRelayer;
+  let initSpy: jest.SpyInstance<Promise<void>, []>;
   const payload = {
     to: '0x0',
     gasLimit: 21000,
@@ -19,12 +25,29 @@ describe('ApiRelayer', () => {
 
   beforeEach(async function () {
     relayer = (new ApiRelayer({ apiKey: 'key', apiSecret: 'secret' }) as unknown) as TestApiRelayer;
+    initSpy = jest.spyOn(relayer, 'init');
   });
 
   describe('constructor', () => {
-    test('calls init', () => {
-      expect(auth.authenticate).toBeCalledWith({ Username: 'key', Password: 'secret' });
-      expect(api.createApi).toBeCalled();
+    test('sets API key and secret', () => {
+      expect(relayer.apiKey).toBe('key');
+      expect(relayer.apiSecret).toBe('secret');
+    });
+
+    test("doesn't call init more than once", async () => {
+      await relayer.sendTransaction(payload);
+      await relayer.sendTransaction(payload);
+      await relayer.sendTransaction(payload);
+
+      expect(initSpy).toBeCalledTimes(1);
+    });
+
+    test('throw an init exception at the correct context', async () => {
+      relayer.init = () => {
+        throw new Error('Init failed');
+      };
+      await expect(relayer.sendTransaction(payload)).rejects.toThrow(/init failed/i);
+      expect(relayer.api).toBe(undefined);
     });
   });
 
@@ -32,6 +55,7 @@ describe('ApiRelayer', () => {
     test('passes correct arguments to the API', async () => {
       await relayer.sendTransaction(payload);
       expect(relayer.api.post).toBeCalledWith('/txs', payload);
+      expect(initSpy).toBeCalled();
     });
   });
 
@@ -39,6 +63,15 @@ describe('ApiRelayer', () => {
     test('passes correct arguments to the API', async () => {
       await relayer.query('42');
       expect(relayer.api.get).toBeCalledWith('txs/42');
+      expect(initSpy).toBeCalled();
+    });
+  });
+
+  describe('sign', () => {
+    test('signs a hex string', async () => {
+      await relayer.sign({ message: '0xdead' });
+      expect(relayer.api.post).toBeCalledWith('/sign', { message: '0xdead' });
+      expect(initSpy).toBeCalled();
     });
   });
 });
