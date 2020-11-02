f6ed51e22f072736712cbdf6608ef5a153aefa32
Igor Yalovoy <to@ylv.io>
2020-11-02 21:06:17 +0300

    Add relayer.getRelayer() to relay-send example

--- examples/defender-relay-send/index.js
+++ examples/defender-relay-send/index.js
@@ -5,10 +5,15 @@ const { Relayer } = require('defender-relay-client');
 const params = { apiKey: process.env.API_KEY, apiSecret: process.env.API_SECRET };
 const relayer = new Relayer(params);
 
+async function get() {
+  const info = await relayer.getRelayer();
+  console.log('Relayer info', info);
+}
+
 async function send() {
   const txResponse = await relayer.sendTransaction({
     to: '0x179810822f56b0e79469189741a3fa5f2f9a7631',
-    value: 1e18 * 2,
+    value: 1,
     speed: 'fast',
     gasLimit: '21000',
   });
@@ -30,6 +35,7 @@ async function query(id) {
     if (process.argv.length > 2) {
       await query(process.argv[2]);
     } else {
+      await get();
       await send();
       await sign('0xdead');
     }
