5eca479461447a468ae27b39daa47ebd9ddd2ec2
Santiago Palladino <spalladino@gmail.com>
2020-11-18 20:09:03 -0300

    Export VERSION for troubleshooting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Relayer, RelayerTransaction, RelayerTransactionPayload } from './relayer';
 export { AutotaskRelayer } from './autotask';
 export { ApiRelayer } from './api';
+
+export const VERSION = require('../package.json').version;
