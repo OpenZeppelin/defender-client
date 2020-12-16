20f75fc4210aec1e4c1c33f0b6c9e356a4097809
Igor Yalovoy <to@ylv.io>
2020-12-17 02:35:06 +0300

    Ethersjs contracts deployment example

--- examples/ethers-signer/index.js
+++ examples/ethers-signer/index.js
@@ -3,16 +3,22 @@ require('dotenv').config();
 const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
 const { ethers } = require('ethers');
 const ERC20Abi = require('./erc20.json');
+const ERC20Bytecode = require('./bytecode.json')[0].data.bytecode.object;
 const ERC20Address = '0x6Ea25933e24320B38fED3a654a92948BECd28915';
 
 async function main() {
   const creds = { apiKey: process.env.API_KEY, apiSecret: process.env.API_SECRET };
-  
+
   const provider = new DefenderRelayProvider(creds);
-  const validUntil = (new Date(Date.now() + 120 * 1000)).toISOString();
+  const validUntil = new Date(Date.now() + 120 * 1000).toISOString();
   const signer = new DefenderRelaySigner(creds, provider, { speed: 'fast', validUntil });
 
-  const erc20 = new ethers.Contract(ERC20Address, ERC20Abi, signer);
+  const factory = new ethers.ContractFactory(ERC20Abi, ERC20Bytecode, signer);
+
+  console.log(`Deploying ERC20 contract`);
+  const erc20 = await factory.deploy(100, { gasLimit: 8000000 });
+  console.log(`Contract deployed at address ${erc20.address}`);
+
   const beneficiary = await ethers.Wallet.createRandom().getAddress();
 
   const addr = await signer.getAddress();
