9f7d72f790611718a6975d8b14e1753a1aea00a8
Santiago Palladino <spalladino@gmail.com>
2021-01-07 20:30:44 -0300

    Add base api client for admin

---
+++ packages/admin/src/api.ts
@@ -0,0 +1,29 @@
+import { BaseApiClient } from "../../base/lib";
+import { ExternalApiCreateProposalRequest as CreateProposalRequest } from "./models/proposal";
+import { ExternalApiProposalResponse as ProposalResponse } from "./models/response";
+
+function getEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) throw new Error(`${name} not set`);
+  return value;
+}
+
+export class AdminClient extends BaseApiClient {  
+  protected getPoolId(): string {
+    return getEnv('DEFENDER_ADMIN_POOL_ID');
+  }
+  
+  protected getPoolClientId(): string {
+    return getEnv('DEFENDER_ADMIN_POOL_CLIENT_ID');
+  }
+
+  protected getApiUrl(): string {
+    return getEnv('DEFENDER_ADMIN_API_URL');
+  }
+
+  public async createProposal(proposal: CreateProposalRequest): Promise<ProposalResponse> {
+    return this.apiCall(async api => {
+      return (await api.post('/proposals', proposal)) as ProposalResponse;
+    });
+  }
+}
---
+++ packages/admin/src/index.ts
@@ -0,0 +1,4 @@
+
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const VERSION = require('../package.json').version;
