3ef781e9a82684f9d7aa02963e80745deeab42b2
Santiago Palladino <santiago@openzeppelin.com>
2021-01-25 19:31:48 -0300

    Add new action proposal example (#26)

---
+++ examples/action-proposal/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const { AdminClient } = require('defender-admin-client');
+
+async function main() {
+  const creds = { apiKey: process.env.ADMIN_API_KEY, apiSecret: process.env.ADMIN_API_SECRET };
+  const client = new AdminClient(creds);
+
+  const proposal = await client.createProposal({
+    contract: {
+      address: '0xf62322658Cb0F51C71Da36637a6846B0967cA264',
+      network: 'rinkeby',
+    },
+    title: 'Set to 42',
+    description: 'Set value to 42',
+    type: 'custom',
+    functionInterface: { name: 'setNumber', inputs: [{ name: '_value', type: 'uint256' }] },
+    functionInputs: ['42'],
+    via: '0xF608FA64c4fF8aDdbEd106E69f3459effb4bC3D1',
+    viaType: 'Gnosis Safe',
+  });
+  
+  const siteUrl = process.env.SITE_URL || 'https://defender.openzeppelin.com';
+  console.log(`${siteUrl}/#/admin/contracts/${proposal.contractId}/proposals/${proposal.proposalId}`);
+}
+
+if (require.main === module) {
+  main().catch(console.error);
+}
