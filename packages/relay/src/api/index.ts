6c2beec817ae2f3e9a9e0e15fe0a471d14835dc3
mok <mok0230@protonmail.com>
2022-04-04 23:19:41 -0700

    rename DEFENDER_RELAY_ADMIN* env variables to DEFENDER_RELAY_CLIENT*

--- packages/relay/src/api/index.ts
+++ packages/relay/src/api/index.ts
@@ -20,15 +20,15 @@ export const RelayerApiUrl = () => process.env.DEFENDER_RELAY_API_URL || 'https:
 
 export class RelayClient extends BaseApiClient {
   protected getPoolId(): string {
-    return process.env.DEFENDER_RELAY_ADMIN_POOL_ID || 'us-west-2_94f3puJWv';
+    return process.env.DEFENDER_RELAY_CLIENT_POOL_ID || 'us-west-2_94f3puJWv';
   }
 
   protected getPoolClientId(): string {
-    return process.env.DEFENDER_RELAY_ADMIN_POOL_CLIENT_ID || '40e58hbc7pktmnp9i26hh5nsav';
+    return process.env.DEFENDER_RELAY_CLIENT_POOL_CLIENT_ID || '40e58hbc7pktmnp9i26hh5nsav';
   }
 
   protected getApiUrl(): string {
-    return process.env.DEFENDER_RELAY_ADMIN_API_URL || 'https://defender-api.openzeppelin.com/';
+    return process.env.DEFENDER_RELAY_CLIENT_API_URL || 'https://defender-api.openzeppelin.com/';
   }
 
   public async get(relayerId: string): Promise<RelayerModel> {
