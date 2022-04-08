eda80d19f9e2d7f2f614a33a9f9b962c03450c2a
mok0230 <92540752+mok0230@users.noreply.github.com>
2022-04-08 12:46:03 -0700

    Support Relayer create, read, and update operations (#92)
    * Support Relayer create, read, and update operations (#92)
    
    * add params to mock relayer response to resolve type error
    
    * move api relayer tests to dedicated file
    
    * rename relay-client example relayer-actions
    
    * add relayer create and update examples
    
    * rename DEFENDER_RELAY_ADMIN* env variables to DEFENDER_RELAY_CLIENT*
    
    * include RELAY_CLIENT env variable config in README
    
    * add sentinel client example config to readme
    
    * remove unavailable param from UpdateRelayerPoliciesRequest
    
    * remove unnecessary dependencies from relay client test
    
    * redefine relayer env vars to align with other client packages
    
    * merge relayer policies update with existing relayer policies
    
    * update RelayerModel interface name to RelayerGetResponse
    
    * update variable name RelayerSignerApiUrl
    
    * change relayer actions to relay signer in readme config comment

---
+++ examples/create-relayer/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const { RelayClient } = require('defender-relay-client');
+
+async function main() {
+  const creds = { apiKey: process.env.ADMIN_API_KEY, apiSecret: process.env.ADMIN_API_SECRET };
+  const client = new RelayClient(creds);
+
+  const createParams = {
+    name: 'MyNewRelayer',
+    network: 'rinkeby',
+    minBalance: BigInt(1e17).toString(),
+    policies: {
+      whitelistReceivers: ['0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B'],
+    },
+  };
+
+  const relayer = await client.create(createParams);
+
+  console.log(relayer);
+}
+
+if (require.main === module) {
+  main().catch(console.error);
+}
--- packages/relay/src/api/index.ts
+++ packages/relay/src/api/index.ts
@@ -4,15 +4,81 @@ import {
   IRelayer,
   JsonRpcResponse,
   ListTransactionsRequest,
-  RelayerModel,
+  RelayerGetResponse,
   RelayerTransaction,
   RelayerTransactionPayload,
   SignedMessagePayload,
   SignTypedDataPayload,
   SignMessagePayload,
+  CreateRelayerRequest,
+  RelayerListResponse,
+  UpdateRelayerPoliciesRequest,
+  UpdateRelayerRequest,
 } from '../relayer';
 
-export const RelayerApiUrl = () => process.env.DEFENDER_RELAY_API_URL || 'https://api.defender.openzeppelin.com/';
+export const RelaySignerApiUrl = () =>
+  process.env.DEFENDER_RELAY_SIGNER_API_URL || 'https://api.defender.openzeppelin.com/';
+
+export class RelayClient extends BaseApiClient {
+  protected getPoolId(): string {
+    return process.env.DEFENDER_RELAY_POOL_ID || 'us-west-2_94f3puJWv';
+  }
+
+  protected getPoolClientId(): string {
+    return process.env.DEFENDER_RELAY_POOL_CLIENT_ID || '40e58hbc7pktmnp9i26hh5nsav';
+  }
+
+  protected getApiUrl(): string {
+    return process.env.DEFENDER_RELAY_API_URL || 'https://defender-api.openzeppelin.com/';
+  }
+
+  public async get(relayerId: string): Promise<RelayerGetResponse> {
+    return this.apiCall(async (api) => {
+      return await api.get(`/relayers/${relayerId}`);
+    });
+  }
+
+  public async list(): Promise<RelayerListResponse> {
+    return this.apiCall(async (api) => {
+      return await api.get('/relayers/summary');
+    });
+  }
+
+  public async create(relayer: CreateRelayerRequest): Promise<RelayerGetResponse> {
+    return this.apiCall(async (api) => {
+      return await api.post('/relayers', relayer);
+    });
+  }
+
+  public async update(relayerId: string, relayerUpdateParams: UpdateRelayerRequest): Promise<RelayerGetResponse> {
+    const currentRelayer = await this.get(relayerId);
+
+    if (relayerUpdateParams.policies) {
+      const updatedRelayer = await this.updatePolicies(relayerId, {
+        ...currentRelayer.policies,
+        ...relayerUpdateParams.policies,
+      });
+      // if policies are the only update, return
+      if (Object.keys(relayerUpdateParams).length === 1) return updatedRelayer;
+    }
+
+    return this.apiCall(async (api) => {
+      return await api.put(`/relayers`, {
+        ...currentRelayer,
+        ...relayerUpdateParams,
+      });
+    });
+  }
+
+  private async updatePolicies(
+    relayerId: string,
+    relayerPolicies: UpdateRelayerPoliciesRequest,
+  ): Promise<RelayerGetResponse> {
+    return this.apiCall(async (api) => {
+      return await api.put(`/relayers/${relayerId}`, relayerPolicies);
+    });
+  }
+}
 
 export class ApiRelayer extends BaseApiClient implements IRelayer {
   private jsonRpcRequestNextId: number;
@@ -23,20 +89,20 @@ export class ApiRelayer extends BaseApiClient implements IRelayer {
   }
 
   protected getPoolId(): string {
-    return process.env.DEFENDER_RELAY_POOL_ID || 'us-west-2_iLmIggsiy';
+    return process.env.DEFENDER_RELAY_SIGNER_POOL_ID || 'us-west-2_iLmIggsiy';
   }
 
   protected getPoolClientId(): string {
-    return process.env.DEFENDER_RELAY_POOL_CLIENT_ID || '1bpd19lcr33qvg5cr3oi79rdap';
+    return process.env.DEFENDER_RELAY_SIGNER_POOL_CLIENT_ID || '1bpd19lcr33qvg5cr3oi79rdap';
   }
 
   protected getApiUrl(): string {
-    return RelayerApiUrl();
+    return RelaySignerApiUrl();
   }
 
-  public async getRelayer(): Promise<RelayerModel> {
+  public async getRelayer(): Promise<RelayerGetResponse> {
     return this.apiCall(async (api) => {
-      return (await api.get('/relayer')) as RelayerModel;
+      return (await api.get('/relayer')) as RelayerGetResponse;
     });
   }
 
