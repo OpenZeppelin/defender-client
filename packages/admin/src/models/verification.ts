aec2baaa4da8f8a705083b5702056ea1b7498bee
Martin Verzilli <martin.verzilli@gmail.com>
2022-08-26 15:12:38 +0200

    Add code to invoke verification through Defender's API (#155)
    * Add code to invoke verification through Defender's API
    
    * Add get verification endpoint
    
    * Add comment on network provenance
    
    * Add providedByType field
    
    * Rename verification methods and lint
    
    * Fix build
    
    Co-authored-by: Santiago Palladino <spalladino@gmail.com>

---
+++ packages/admin/src/models/verification.ts
@@ -0,0 +1,27 @@
+// These types are vendored from the models package
+import { Network } from 'defender-base-client';
+
+export type Address = string;
+
+export interface VerificationRequest {
+  artifactUri: string;
+  solidityFilePath: string;
+  contractName: string;
+  contractAddress: Address;
+  contractNetwork: Network;
+}
+
+export type Verification = VerificationRequest & {
+  verificationId: string;
+  artifactUri: string;
+  solidityFilePath: string;
+  contractName: string;
+  contractAddress: Address;
+  contractNetwork: Network;
+  onChainSha256: string;
+  providedSha256: string;
+  lastVerifiedAt: string;
+  matchType: 'NO_MATCH' | 'PARTIAL' | 'EXACT';
+  providedBy: string;
+  providedByType: 'USER_EMAIL' | 'API_KEY';
+};
