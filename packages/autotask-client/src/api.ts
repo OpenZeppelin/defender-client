0134802ffc59060e1bb2ffadc5ef4e680f46adae
ShahNami <me@nami.sh>
2022-08-30 14:38:20 +0100

    remove override url

--- packages/autotask-client/src/api.ts
+++ packages/autotask-client/src/api.ts
@@ -116,7 +116,7 @@ export class AutotaskClient extends BaseApiClient {
 
   public async createSecrets(data: SaveSecretsRequest): Promise<GetSecretsResponse> {
     return this.apiCall(async (api) => {
-      return await api.post(`/secrets`, data);
-    }, process.env.DEFENDER_SECRETS_API_URL || 'https://defender-api.openzeppelin.com/secrets/');
+      return await api.post(`/autotasks/secrets`, data);
+    });
   }
 }
--- packages/base/src/api/client.ts
+++ packages/base/src/api/client.ts
@@ -18,24 +18,24 @@ export abstract class BaseApiClient {
     this.apiSecret = params.apiSecret;
   }
 
-  protected async init(overrideApiUrl?: string): Promise<AxiosInstance> {
+  protected async init(): Promise<AxiosInstance> {
     if (!this.api) {
       const userPass = { Username: this.apiKey, Password: this.apiSecret };
       const poolData = { UserPoolId: this.getPoolId(), ClientId: this.getPoolClientId() };
-      this.api = createAuthenticatedApi(userPass, poolData, overrideApiUrl ?? this.getApiUrl());
+      this.api = createAuthenticatedApi(userPass, poolData, this.getApiUrl());
     }
     return this.api;
   }
 
-  protected async apiCall<T>(fn: (api: AxiosInstance) => Promise<T>, overrideApiUrl?: string): Promise<T> {
-    const api = await this.init(overrideApiUrl);
+  protected async apiCall<T>(fn: (api: AxiosInstance) => Promise<T>): Promise<T> {
+    const api = await this.init();
     try {
       return await fn(api);
     } catch (error) {
       // this means ID token has expired so we'll recreate session and try again
       if (error.response && error.response.status === 401 && error.response.statusText === 'Unauthorized') {
         this.api = undefined;
-        const api = await this.init(overrideApiUrl);
+        const api = await this.init();
         return await fn(api);
       }
       throw error;
