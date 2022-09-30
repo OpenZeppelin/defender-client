603823092270e4967424fc2595a8237508321556
ernestognw <ernestognw@gmail.com>
2022-09-30 18:42:55 -0500

    Added Batch proposals support

--- packages/admin/src/models/proposal.ts
+++ packages/admin/src/models/proposal.ts
@@ -1,17 +1,16 @@
 import { Network } from 'defender-base-client';
 
 // Copied from openzeppelin/defender/models/src/types/proposal-api.req.d.ts
+export type Hex = string;
 export type Address = string;
-export type ProposalType = 'upgrade' | 'custom' | 'pause';
+export type BigUInt = string | number;
+export type ProposalType = ProposalStepType | ProposalBatchType;
+export type ProposalStepType = 'upgrade' | 'custom' | 'pause' | 'send-funds' | 'access-control';
+export type ProposalBatchType = 'batch';
 export type ProposalFunctionInputs = (string | boolean | (string | boolean)[])[];
 
 export interface ExternalApiCreateProposalRequest {
-  contract: {
-    network: Network;
-    address: Address;
-    name?: string;
-    abi?: string;
-  };
+  contract: PartialContract | PartialContract[];
   title: string;
   description: string;
   type: ProposalType;
@@ -20,13 +19,35 @@ export interface ExternalApiCreateProposalRequest {
   viaType?: 'EOA' | 'Gnosis Safe' | 'Gnosis Multisig';
   functionInterface?: ProposalTargetFunction;
   functionInputs?: ProposalFunctionInputs;
+  steps?: ProposalStep[];
 }
+
+export interface PartialContract {
+  network: Network;
+  address: Address;
+  name?: string;
+  abi?: string;
+}
+
+export interface ProposalStep {
+  contractId: string;
+  targetFunction?: ProposalTargetFunction;
+  functionInputs?: ProposalFunctionInputs;
+  metadata?: ProposalMetadata;
+  type: ProposalStepType;
+}
+
 export interface ProposalMetadata {
   newImplementationAddress?: Address;
   newImplementationAbi?: string;
   proxyAdminAddress?: Address;
-  action?: 'pause' | 'unpause';
+  action?: 'pause' | 'unpause' | 'grantRole' | 'revokeRole';
   operationType?: 'call' | 'delegateCall';
+  account?: Address;
+  role?: Hex;
+  sendTo?: Address;
+  sendValue?: BigUInt;
+  sendCurrency?: Token | NativeCurrency;
 }
 export interface ProposalTargetFunction {
   name?: string;
@@ -37,4 +58,19 @@ export interface ProposalFunctionInputType {
   type: string;
   internalType?: string;
   components?: ProposalFunctionInputType[];
+}
+
+export interface Token {
+  name: string;
+  symbol: string;
+  address: Address;
+  network: Network;
+  decimals: number;
+  type: 'ERC20';
+}
+export interface NativeCurrency {
+  name: string;
+  symbol: string;
+  decimals: number;
+  type: 'native';
 }
