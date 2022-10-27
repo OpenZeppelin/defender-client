95e1a681a4b259c03c6d43bb7c2d7a1e5c95221d
Manboy <2156509+manboy-eth@users.noreply.github.com>
2022-10-27 18:38:25 +0200

    Export useful types from the autotask client (#174)

--- packages/autotask-client/src/index.ts
+++ packages/autotask-client/src/index.ts
@@ -1,4 +1,12 @@
 export { AutotaskClient } from './api';
+export {
+  CreateAutotaskRequest,
+  UpdateAutotaskRequest,
+  GetSecretsResponse,
+  SaveSecretsRequest,
+} from './models/autotask';
+export { AutotaskRunBase, AutotaskRunListResponse, AutotaskRunResponse } from './models/autotask-run.res';
+export { AutotaskDeleteResponse, AutotaskListResponse, AutotaskResponse } from './models/response';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const VERSION = require('../package.json').version;
