b1f263171074cb473c33f5d71f09436b0692fc12
dylankilkenny <dylankilkenny95@gmail.com>
2023-01-12 13:58:24 +0000

    remove module name

--- packages/deploy/src/api/platform.ts
+++ packages/deploy/src/api/platform.ts
@@ -2,14 +2,14 @@ import { BaseApiClient } from 'defender-base-client';
 
 export class PlatformApiClient extends BaseApiClient {
   protected getPoolId(): string {
-    return process.env.PLATFORM_DEPLOYMENT_POOL_ID || 'us-west-2_94f3puJWv';
+    return process.env.PLATFORM_POOL_ID || 'us-west-2_94f3puJWv';
   }
 
   protected getPoolClientId(): string {
-    return process.env.PLATFORM_DEPLOYMENT_POOL_CLIENT_ID || '40e58hbc7pktmnp9i26hh5nsav';
+    return process.env.PLATFORM_CLIENT_ID || '40e58hbc7pktmnp9i26hh5nsav';
   }
 
   protected getApiUrl(): string {
-    return process.env.PLATFORM_DEPLOYMENT_API_URL || 'https://defender-api.openzeppelin.com/deployment/';
+    return process.env.PLATFORM_API_URL || 'https://defender-api.openzeppelin.com/deployment/';
   }
 }
