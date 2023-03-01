9e5e731423625d4bc524bd5d3b5a405d9ef470b1
Nami <me@nami.sh>
2023-03-01 16:27:52 +0100

    Add autotask `monitor-filter` trigger type (#207)
    * add autotask trigger type
    
    * remove yarn changes

--- packages/autotask-utils/src/types.ts
+++ packages/autotask-utils/src/types.ts
@@ -1,7 +1,7 @@
 export type SentinelConfirmation = number | 'safe' | 'finalized';
 
 export interface PreviousAutotaskRunInfo {
-  trigger: 'schedule' | 'webhook' | 'sentinel' | 'manual' | 'manual-api';
+  trigger: 'schedule' | 'webhook' | 'sentinel' | 'monitor-filter' | 'manual' | 'manual-api';
   status: 'pending' | 'throttled' | 'error' | 'success';
   createdAt: string;
   autotaskId: string;
