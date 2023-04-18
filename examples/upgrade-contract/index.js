9e9fd491fc18805fc9a6bd01eb30282e07885850
ShahNami <me@nami.sh>
2023-04-18 12:11:36 +0100

    comments

--- examples/upgrade-contract/index.js
+++ examples/upgrade-contract/index.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 
 const { PlatformClient } = require('platform-deploy-client');
 
-const abiFile = require('./abis/Box.json');
-
+const boxAbiFile = require('./abis/Box.json');
+// const tokenAbiFile = require('./abis/Box.json');
 
 async function main() {
     const creds = { apiKey: process.env.ADMIN_API_KEY, apiSecret: process.env.ADMIN_API_SECRET };
     const client = new PlatformClient(creds);
 
-    // Get approval process for upgrade on Goerli
+    // Get approval process for deployment on Goerli
     const config = await client.Upgrade.getApprovalProcess("goerli");
     console.log(config);
 
     const upgrade = await client.Upgrade.upgrade({
         proxyAddress: "0xABC1234...",
         proxyAdminAddress: "0xDEF1234...",
-        newImplementationABI: JSON.stringify(abiFile),
+        newImplementationABI: JSON.stringify(boxAbiFile),
         newImplementationAddress: "0xABCDEF1....",
         network: 'goerli',
     });
