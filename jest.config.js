323f16ba326ff5071ded447ee49b288efb673cf6
Sai Chaitanya Tirumerla <tirumerla@gmail.com>
2023-04-27 19:12:03 -0700

    Add code coverage

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
       diagnostics: true,
     },
   },
+  collectCoverage : true,
+  collectCoverageFrom: ['packages/**/src/**/*.{js,ts}', 'packages/**/fixtures/**/*.{js.ts}', '!**/node_modules/**'],
+  //coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
 };
