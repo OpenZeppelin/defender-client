5835e7e3c494e0797d4770b42ddcd72e1c7e9d4f
Eric Lau <ericglau@outlook.com>
2023-05-04 14:10:22 -0400

    Export UpgradeClient in deploy package

--- packages/deploy/src/index.ts
+++ packages/deploy/src/index.ts
@@ -2,6 +2,7 @@ export { PlatformClient } from './api';
 export { BlockExplorerApiKeyClient } from './api/block-explorer-api-key';
 export { DeploymentClient } from './api/deployment';
 export { DeploymentConfigClient } from './api/deployment-config';
+export { UpgradeClient } from './api/upgrade';
 export * from './models';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
