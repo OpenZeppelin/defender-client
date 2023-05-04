382f13f68ef40f8f6f75b867ebfd054798b595bd
ShahNami <me@nami.sh>
2023-05-04 20:06:45 +0100

    Expose upgrade client

--- packages/deploy/src/index.ts
+++ packages/deploy/src/index.ts
@@ -1,7 +1,9 @@
 export { PlatformClient } from './api';
 export { BlockExplorerApiKeyClient } from './api/block-explorer-api-key';
 export { DeploymentClient } from './api/deployment';
+export { UpgradeClient } from './api/upgrade';
 export { DeploymentConfigClient } from './api/deployment-config';
+
 export * from './models';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
