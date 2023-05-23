e3d8245cd15c1115dbeb1c7aeac2425e87a244d9
Sai Chaitanya Tirumerla <tirumerla@gmail.com>
2023-05-23 14:06:32 -0700

    Use beta tags for rc

--- examples/create-sentinel/index.js
+++ examples/create-sentinel/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 
 const abi = require('./abis/erc721.json');
--- examples/update-notification-category/index.js
+++ examples/update-notification-category/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 
 const { SentinelClient } = require('defender-sentinel-client');
