c07941c34f2467e8c3bf18ca3fde2eb838cc7710
Marcos <marcoscarlomagno1@gmail.com>
2023-11-06 11:40:41 -0300

    improve pagination example

--- examples/list-proposals/index.js
+++ examples/list-proposals/index.js
@@ -6,12 +6,25 @@ async function main() {
 	const creds = { apiKey: process.env.ADMIN_API_KEY, apiSecret: process.env.ADMIN_API_SECRET };
 	const client = new AdminClient(creds);
 
-	const proposals = await client.listProposals({
-		limit: 10,
-		next: undefined,
-	});
+	const allProposals = await client.listProposals();
 
-	console.log(proposals);
+	if (allProposals.length > 1) {
+		// pagination example
+		const firstPage = await client.listProposals({
+			limit: 1,
+			next: undefined,
+		});
+
+		const secondPage = await client.listProposals({
+			limit: 1,
+			next: firstPage.next,
+		});
+
+		console.log('First page:', firstPage.items);
+		console.log('Second page:', secondPage.items);
+	}
+
+	console.log('All proposals:', allProposals);
 }
 
 if (require.main === module) {
