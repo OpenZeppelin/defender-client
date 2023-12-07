8fff37e565b1ee370e668e4204f4fab01a293348
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2023-12-07 14:46:11 -0300

    add base sepolia (#455)

--- packages/base/src/utils/network.ts
+++ packages/base/src/utils/network.ts
@@ -37,6 +37,7 @@ type PublicNetwork =
   | 'zksync-goerli'
   | 'base'
   | 'base-goerli'
+  | 'base-sepolia'
   | 'linea'
   | 'linea-goerli'
   | 'mantle'
@@ -86,6 +87,7 @@ export const Networks: Network[] = [
   'zksync-goerli',
   'base',
   'base-goerli',
+  'base-sepolia',
   'linea',
   'linea-goerli',
   'x-dfk-avax-chain',
@@ -145,6 +147,7 @@ const chainIds: { [key in Network]: number } = {
   'zksync-goerli': 280,
   'base': 8453,
   'base-goerli': 84531,
+  'base-sepolia': 84532,
   'linea': 59144,
   'linea-goerli': 59140,
   'x-dfk-avax-chain': 53935,
