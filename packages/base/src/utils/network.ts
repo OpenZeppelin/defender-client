eb821205cd1f6c8908c7fbbdbe552fbb69874010
Marcos <marcoscarlomagno1@gmail.com>
2024-01-10 18:00:19 -0300

    add support to matic zkevm

--- packages/base/src/utils/network.ts
+++ packages/base/src/utils/network.ts
@@ -16,6 +16,8 @@ type PublicNetwork =
   | 'moonbeam'
   | 'matic'
   | 'mumbai'
+  | 'matic-zkevm'
+  | 'matic-zkevm-testnet'
   | 'avalanche'
   | 'fuji'
   | 'optimism'
@@ -126,6 +128,8 @@ const chainIds: { [key in Network]: number } = {
   'moonbeam': 1284,
   'matic': 137,
   'mumbai': 80001,
+  'matic-zkevm': 1101,
+  'matic-zkevm-testnet': 1442,
   'avalanche': 0xa86a,
   'fuji': 0xa869,
   'optimism': 10,
